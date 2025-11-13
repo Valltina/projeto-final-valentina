52bb0708261589f85f21746cca7082335cd5d6aa
Valentina de Oliveira Mendes <valentinaoliveira918@gmail.com>
2025-11-13 01:58:05 +0000

    Tela de Inicio

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,29 +1,41 @@
 
 import FontAwesome from '@expo/vector-icons/FontAwesome';
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
 import Entypo from '@expo/vector-icons/Entypo';
-import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 import { Tabs } from 'expo-router';
+
 
 export default function Layout() {
     return (
-           <Tabs screenOptions={{ tabBarActiveTintColor: 'purple' , headerShown: false,}}>
+           <Tabs screenOptions={{ 
+            tabBarActiveTintColor: '#420703ff' , 
+            tabBarInactiveTintColor: '#e7c077ff',
+            headerShown: false,
+            tabBarStyle: {
+        backgroundColor: '#9c2323ff', 
+    }
+            }}>
       <Tabs.Screen
         name="index" options={{title: 'Inicio',
           tabBarIcon: ({ color }) => <Entypo size={28} name="home" color={color} />,
         }}
       />
 
-       <Tabs.Screen
-        name="pageone" options={{title: 'Obrigatorio',
-          tabBarIcon: ({ color }) => <MaterialCommunityIcons size={28} name="exclamation-thick" color={color} />,
+      <Tabs.Screen
+        name="pagetwo" options={{title: 'Livre',
+          tabBarIcon: ({ color }) => <MaterialIcons name="videocam" size={28} color={color} />,
         }}
       />
 
+
        <Tabs.Screen
-        name="pagetwo" options={{title: 'Livre',
-          tabBarIcon: ({ color }) => <FontAwesome size={28} name="puzzle-piece" color={color} />,
+        name="pageone" options={{title: 'Obrigatorio',
+          tabBarIcon: ({ color }) => <FontAwesome5 name="film" size={24} color={color} />,
         }}
       />
+
+       
 
       <Tabs.Screen
         name="aboutme" options={{title: 'Sobre Mim',
@@ -31,5 +43,7 @@ export default function Layout() {
         }}
       />
     </Tabs>
-    );
+    ); 
+    
 }
+
--- app/index.js
+++ app/index.js
@@ -1,11 +1,21 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
+import logo from "../assets/Cine.png"
 
 export default function Page() {
   return (
     <View style={styles.container}>
       <View style={styles.main}>
-        <Text style={styles.title}>Inicio</Text>
-        <Text style={styles.subtitle}>Projeto de DDM</Text>
+         <Image source={logo} style={styles.logo} />
+
+      </View>
+      <View style={styles.Caixa}>
+        <Text style={styles.TituloCaixa}>
+          Bem vindo(a)</Text>
+          <Text style={styles.TextoCaixa}>
+          Este aplicativo foi desenvolvido para fins pedagógicos como um trabalho escolar sobre o
+           universo do cinema. Explore informações detalhadas e curiosidades sobre filmes e o papel 
+           histórico das películas 35mm.</Text>
+        
       </View>
     </View>
   );
@@ -16,20 +26,41 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     padding: 24,
-    backgroundColor: "#e6cbffff" ,
+    backgroundColor: "#f8edd4ff" ,
+    flexDirection: 'column',
+    paddingTop: 40,
   },
+
   main: {
-    flex: 1,
-    justifyContent: "center",
     maxWidth: 960,
     marginHorizontal: "auto",
+    alignItems: 'center',
   },
-  title: {
-    fontSize: 64,
-    fontWeight: "bold",
+ 
+   logo: {
+    height: 128,
+    width: 128,
   },
-  subtitle: {
-    fontSize: 36,
-    color: "#38434D",
+
+  Caixa: {
+    backgroundColor: '#e7c077ff', 
+    padding: 20, 
+    borderRadius: 25, 
+    width: '90%',
+    marginTop: '50',
+    borderWidth: 2,
+    borderColor: '#cc9837ff',
+   
+  },
+  
+  TextoCaixa: {
+    color: '#ffffffff', 
+    fontSize: 20,
+    textAlign: 'center',
+  },
+  TituloCaixa: {
+    color: '#ffffffff',
+    fontSize: 30,
+    textAlign: 'center', 
   },
 });
